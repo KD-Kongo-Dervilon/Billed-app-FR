d44d2917e96517abfe08e32e9e1f3a488a6f8ff0
Tydis KD <82412416+KD-Kongo-Dervilon@users.noreply.github.com>
2022-01-30 23:25:58 +0100

    Fix  dashboard bug hunt
    Update Dashboard.js

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -82,7 +82,7 @@ export default class {
     const billUrl = $('#icon-eye-d').attr("data-bill-url")
     const imgWidth = Math.floor($('#modaleFileAdmin1').width() * 0.8)
     $('#modaleFileAdmin1').find(".modal-body").html(`<div style='text-align: center;'><img width=${imgWidth} src=${billUrl} /></div>`)
-    if (typeof $('#modaleFileAdmin1').modal === 'function') $('#modaleFileAdmin1').modal('show')
+    $('#modaleFileAdmin1').modal('show')
   }
 
   handleEditTicket(e, bill, bills) {
@@ -131,27 +131,29 @@ export default class {
   }
 
   handleShowTickets(e, bills, index) {
-    if (this.counter === undefined || this.index !== index) this.counter = 0
-    if (this.index === undefined || this.index !== index) this.index = index
-    if (this.counter % 2 === 0) {
-      $(`#arrow-icon${this.index}`).css({ transform: 'rotate(0deg)'})
-      $(`#status-bills-container${this.index}`)
-        .html(cards(filteredBills(bills, getStatus(this.index))))
-      this.counter ++
-    } else {
-      $(`#arrow-icon${this.index}`).css({ transform: 'rotate(90deg)'})
-      $(`#status-bills-container${this.index}`)
-        .html("")
-      this.counter ++
-    }
+		if (this.counter === undefined || this.index !== index) this.counter = 0;
+		if (this.index === undefined || this.index !== index) this.index = index;
+		if (this.counter % 2 === 0) {
+			$(`#arrow-icon${this.index}`).css({ transform: "rotate(0deg)" });
+			$(`#status-bills-container${this.index}`).html(
+				cards(filteredBills(bills, getStatus(this.index)))
+			);
+			this.counter++;
+		} else {
+			$(`#arrow-icon${this.index}`).css({ transform: "rotate(90deg)" });
+			$(`#status-bills-container${this.index}`).html("");
+			this.counter++;
+		}
+const currentFilteredBills = filteredBills(bills, getStatus(this.index));
+		currentFilteredBills.forEach((bill) => {
+			$(`#open-bill${bill.id}`)
+				.off("click")
+				.on("click", (e) => this.handleEditTicket(e, bill, bills));
+		});
 
-    bills.forEach(bill => {
-      $(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
-    })
-
-    return bills
-
-  }
+		
+		return bills;
+	}
 
   // not need to cover this function by tests
   getBillsAllUsers = () => {
